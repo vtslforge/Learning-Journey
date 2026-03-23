c5d8463fce2f9f0dcbff7eeb6bb1fd1cf280acd4
Vatsalya Singh <singhvatsalya16@gmail.com>
2026-03-24 00:03:47 +0530

    learning

--- User_manager_app/frontend/src/App.jsx
+++ User_manager_app/frontend/src/App.jsx
@@ -1,22 +1,45 @@
-import React from 'react'
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home  from "./Home";
-import UserManager from './UserManager';
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 
-
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element:<Home/>,
-  },
-  {
-    path :"/user-manager",
-    element:<UserManager/>
-  },
-]);
 const App = () => {
-  return <RouterProvider router={router} />;
+  const {register,handleSubmit,formState: { errors },} = useForm();
+  const [result, setResult] = useState(null);
+  const onSubmit = (data) => console.log(data);
+  // api form companyDB
+  const handleapi = async () => {
+    const response = await fetch("http://localhost:3001/api/data");
+    const data = await response.json();
+    setResult(data);
+    console.log(data);
+  };
+
+  return (
+    <div className="text-white">
+      <button onClick={handleapi}>tap</button>
+      {result?.departments?.map((dept, index) => (
+        <p key={index}>{dept.name}</p>
+      ))}
+      {/* handling form */}
+      <div className="text-white  ">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input
+            className="border border-white"
+            defaultValue="test"
+            {...register("example")}
+          />
+          <input
+            className="border border-white"
+            {...register("exampleRequired", { required: true })}
+          />
+          {errors.exampleRequired && <span>This field is required</span>}
+
+          <input type="submit" />
+
+        </form>
+        <p>{}</p>
+      </div>
+    </div>
+  );
 };
 
-export default App 
+export default App;
