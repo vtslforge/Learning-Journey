90a7638add8a61e59699f7d10aebfad72c6a791d
vatsalya singh <singhvatsalya16@gmail.com>
2026-06-04 23:53:33 +0530

    function & any/unknown

---
+++ TS/src/function.ts
@@ -0,0 +1,14 @@
+function cars() {
+    return "lambo"
+}
+
+function fruit() {
+    
+}
+
+function sayHello():void {
+console.log("Hello");
+}
+
+let typeinfer = sayHello()
+console.log(typeinfer)
--- TS/src/tuple.ts
+++ TS/src/tuple.ts
@@ -19,3 +19,17 @@ let appData:[appdataInfo] = [
 appData.forEach(element => {
     console.log(element)
 });
+console.log("-------------------------------------------------")
+// another example - 
+type tpltype = {
+    name: string
+    id:number
+    value:boolean
+}
+let tplexample:[tpltype] = [
+    {name:"vatsalya" , id : 23 , value:false}
+]
+
+tplexample.forEach(num => {
+    console.log(num)
+});
