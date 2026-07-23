fe5797242a2cef7ca393485a9112f315b228ec06
vatsalya singh <singhvatsalya16@gmail.com>
2026-07-23 23:11:23 +0530

    starting java spring boot

---
+++ Java/test/src/App.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+        System.out.println(age);
+    }
+}
